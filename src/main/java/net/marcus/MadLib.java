31853e5c6f1f8306b62059587978457f2819679b
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2016-08-09 22:44:51 +0100

    Testing the input number and type

--- src/main/java/net/marcus/MadLib.java
+++ src/main/java/net/marcus/MadLib.java
@@ -1,23 +1,55 @@
 package net.marcus;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MadLib {
 
 	private Scanner scanner;
+	private int successfullyPromptedTimes = 0;
+	private static final Pattern WORD_PATTERN = Pattern.compile("[a-zA-Z]+(?:\\-[a-zA-Z]+)?");
+	private String words[] = new String [4];
+	private static final String WORDS[] = {"noun", "verb", "adjective", "adverb"}; 
 
 	public MadLib(InputStream inputStream) {
 		scanner = new Scanner(inputStream);
 	}
 
-	public int getSuccessfullyPromptedTimes() {
-		int i;
-		String text;
-		for(i = 0; i < 4; i++){
-			text = scanner.next();
-		}
-		return i;
+
+	public void playMadLib() {
+		retrieveInputs();
 	}
 
+	private void retrieveInputs() {
+		for(int i = 0; i < 4; i++){
+			words[i] = getWord(i); 
+		}
+	}
+
+	private String getWord(int num) {
+		String word;
+		do {
+			System.out.println("Type a "+WORDS[num]+": ");
+			word = scanner.next();
+		} while(isNotAWord(word));
+		
+		successfullyPromptedTimes++;
+		return word;
+	}
+
+
+	private boolean isNotAWord(String text) {
+		return !WORD_PATTERN.matcher(text).matches();
+	}
+
+	public int getSuccessfullyPromptedTimes() {
+		return successfullyPromptedTimes;
+	}
+	
+	public String[] getWords(){
+		return words;
+	}
 }
--- src/test/java/net/marcus/MadLibTest.java
+++ src/test/java/net/marcus/MadLibTest.java
@@ -1,32 +1,28 @@
 package net.marcus;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.Ignore;
 
 public class MadLibTest {
 
-	private MadLib madLib;
-	private PipedInputStream pipedInputStream;
-	private PipedOutputStream pipedOutputStream;
-	
-	@Before
-	public void setUp() throws Exception {
-		pipedInputStream = new PipedInputStream();
-		pipedOutputStream = new PipedOutputStream(pipedInputStream);
-		madLib = new MadLib(pipedInputStream);
-	}
+	private static final int TIMEOUT = 5000;
 
 	@Test
 	public void shouldTestUserPromptedSuccessfully4Times() {
-		try {
-			pipedOutputStream.write("fake.noun fake.verb fake.adjective fake.adverb\n".getBytes());
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("fake-noun fake-verb fake-adjective fake-adverb\n".getBytes());
+			
+			madLib.playMadLib();
 			assertEquals(4, madLib.getSuccessfullyPromptedTimes());
 		} catch (IOException e) {
 			fail(e.getMessage());
@@ -34,22 +30,161 @@ public class MadLibTest {
 	}
 
 	@Test
-	@Ignore
-	public void shouldTestUserPromptedSuccessfully3Times() {
-		try {
-			pipedOutputStream.write("fake.noun fake.verb fake.adjective\n".getBytes());
+	public void shouldBlockWhenUserInputed3Times() {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("fake-noun fake-verb fake-adjective\n".getBytes());
+			
+//			Thread t = new Thread(new Runnable() {
+//			    public void run() {
+//		    		madLib.playMadLib();
+//			    }
+//			});
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
 			assertEquals(3, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
+		} catch (IOException | InterruptedException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void shouldBlockWhenUserInputedNilTimes() throws Exception {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("\n".getBytes());
+			
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
+			assertEquals(0, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
 		} catch (IOException e) {
 			fail(e.getMessage());
 		}
 	}
 	
 	@Test
-	@Ignore 
-	public void shouldTestUserPromptedSuccessfullyNilTimes() throws Exception {
-		try {
-			pipedOutputStream.write("\n".getBytes());
+	public void shouldBlockWhenFirstInputNotAString() throws Exception {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("-1\n".getBytes());
+			
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
 			assertEquals(0, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void shouldBlockWhenSecondInputNotAString() throws Exception {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("fake-noun -666\n".getBytes());
+			
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
+			assertEquals(1, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void shouldBlockWhenThirdInputNotAString() throws Exception {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("fake-noun fake-verb +567\n".getBytes());
+			
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
+			assertEquals(2, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void shouldBlockWhenFourthInputNotAString() throws Exception {
+		MadLib madLib;
+		try (PipedInputStream pipedInputStream = new PipedInputStream();
+				PipedOutputStream pipedOutputStream = new PipedOutputStream(pipedInputStream)) {
+			madLib = new MadLib(pipedInputStream);
+			
+			pipedOutputStream.write("fake-noun fake-verb fake-adjective 9090\n".getBytes());
+			
+			Thread t = new Thread( () -> madLib.playMadLib() );
+			
+			long startTime = System.currentTimeMillis();
+			t.start();
+			t.join(TIMEOUT);
+			
+			assertEquals(true, System.currentTimeMillis() - startTime >= TIMEOUT);
+			assertEquals(true, t.isAlive());
+			assertEquals(3, madLib.getSuccessfullyPromptedTimes());
+
+			t.interrupt();
+			t.join();
 		} catch (IOException e) {
 			fail(e.getMessage());
 		}
