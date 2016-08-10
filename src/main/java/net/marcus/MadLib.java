b25a93f301e9b29723add0002a7571da1cf9f781
Marc Sabates Gelpi <marc.sabates.gelpi.workspace@gmail.com>
2016-08-10 19:45:40 +0100

    The program working

--- src/main/java/net/marcus/MadLib.java
+++ src/main/java/net/marcus/MadLib.java
@@ -1,8 +1,6 @@
 package net.marcus;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -12,7 +10,13 @@ public class MadLib {
 	private int successfullyPromptedTimes = 0;
 	private static final Pattern WORD_PATTERN = Pattern.compile("[a-zA-Z]+(?:\\-[a-zA-Z]+)?");
 	private String words[] = new String [4];
-	private static final String WORDS[] = {"noun", "verb", "adjective", "adverb"}; 
+	private static final String WORDS[] = {"noun", "verb", "adjective", "adverb"};
+	private static final String MAD_PHRASE = "Do you %s your %s %s %s? That's hilarious!";
+	
+	public static void main(String args[]){
+		MadLib madlib = new MadLib(System.in);
+		madlib.playMadLib();
+	}
 
 	public MadLib(InputStream inputStream) {
 		scanner = new Scanner(inputStream);
@@ -21,6 +25,12 @@ public class MadLib {
 
 	public void playMadLib() {
 		retrieveInputs();
+		printMadPhrase();
+	}
+
+	private void printMadPhrase() {
+		System.out.println(String.format(MAD_PHRASE, words[1], words[2], words[0], words[3]));
+		
 	}
 
 	private void retrieveInputs() {
@@ -32,7 +42,7 @@ public class MadLib {
 	private String getWord(int num) {
 		String word;
 		do {
-			System.out.println("Type a "+WORDS[num]+": ");
+			System.out.print("Type a "+WORDS[num]+": ");
 			word = scanner.next();
 		} while(isNotAWord(word));
 		
